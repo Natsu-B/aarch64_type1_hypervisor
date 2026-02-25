e91ef6b5c7dc4c2efec475203db38160a9cfa938
Natsu-B <natsu.minatomirai@gmail.com>
2026-02-26 00:46:32 +0900

    feat(bootloader): wire rpi4_net Ethernet init to BCM2711 GENETv5 backend

--- bootloader/src/net/rpi4.rs
+++ bootloader/src/net/rpi4.rs
@@ -1,9 +1,14 @@
+use arch_hal::soc::bcm2711::genet::Bcm2711GenetV5;
 use dtb::DtbParser;
 use io_api::ethernet::EthernetFrameIo;
 
 /// Initializes and returns the board Ethernet frame I/O backend from DTB data.
 ///
-/// This is currently a placeholder until the RPi4 NIC driver is wired.
-pub fn init_ethernet_from_dtb(_dtb: &DtbParser) -> &'static mut dyn EthernetFrameIo {
-    unimplemented!("rpi4_net enabled but NIC driver not wired yet")
+/// This is used by UDP UART networking setup in `bootloader/src/main.rs` when
+/// `feature = \"rpi4_net\"` is enabled. Boot-time failure is treated as fatal.
+pub fn init_ethernet_from_dtb(dtb: &DtbParser) -> &'static mut dyn EthernetFrameIo {
+    match Bcm2711GenetV5::init_from_dtb(dtb) {
+        Ok(driver) => driver as &'static mut dyn EthernetFrameIo,
+        Err(err) => panic!("rpi4_net: failed to init BCM2711 GENETv5: {:?}", err),
+    }
 }
